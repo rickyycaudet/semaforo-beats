aa95eb072bea6ffffe3707f1abc5b12262496d02
rickyycaudet <rickyycaudet@gmail.com>
2026-03-01 18:03:23 +0100

    Create page.tsx

---
+++ src/app/estadisticas/page.tsx
@@ -0,0 +1,10 @@
+export default function EstadisticasPage() {
+  return (
+    <div>
+      <h1 style={{ fontSize: 28, fontWeight: 800 }}>Estadísticas</h1>
+      <p style={{ marginTop: 8 }}>
+        Aquí verás beneficio/pérdida, ROI, winrate, drawdown y rendimiento por color (🟢🟠🔴).
+      </p>
+    </div>
+  );
+}
