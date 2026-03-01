cc4b3830f36a77bc3b568e6fe155fb4000dea19b
rickyycaudet <rickyycaudet@gmail.com>
2026-03-02 00:40:55 +0100

    Update page.tsx

--- src/app/ajustes/page.tsx
+++ src/app/ajustes/page.tsx
@@ -1,13 +1,16 @@
 import AuthGate from "../../components/AuthGate";
+import AjustesClient from "../../components/AjustesClient";
 
 export default function AjustesPage() {
   return (
     <AuthGate>
       <div>
         <h1 style={{ fontSize: 28, fontWeight: 800 }}>Ajustes</h1>
-        <p style={{ marginTop: 8 }}>
-          Aquí configurarás tu bankroll y el modo de staking.
+        <p style={{ marginTop: 8, marginBottom: 14 }}>
+          Configura tu bankroll inicial y el staking (manual o Kelly fraccional).
         </p>
+
+        <AjustesClient />
       </div>
     </AuthGate>
   );
