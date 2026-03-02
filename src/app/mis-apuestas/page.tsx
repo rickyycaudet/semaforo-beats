561a1d07da140f6e72aa80da50f945032e0862b1
rickyycaudet <rickyycaudet@gmail.com>
2026-03-02 01:26:55 +0100

    Update page.tsx

--- src/app/mis-apuestas/page.tsx
+++ src/app/mis-apuestas/page.tsx
@@ -1,13 +1,16 @@
 import AuthGate from "../../components/AuthGate";
+import MisApuestasClient from "../../components/MisApuestasClient";
 
 export default function MisApuestasPage() {
   return (
     <AuthGate>
       <div>
         <h1 style={{ fontSize: 28, fontWeight: 800 }}>Mis apuestas</h1>
-        <p style={{ marginTop: 8 }}>
-          Aquí verás todas tus apuestas (pendientes, ganadas, perdidas).
+        <p style={{ marginTop: 8, marginBottom: 14 }}>
+          Añade apuestas y revisa tu historial.
         </p>
+
+        <MisApuestasClient />
       </div>
     </AuthGate>
   );
