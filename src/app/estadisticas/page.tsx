61f86d2f3122937ed6ff8717d7275e59903efc5c
rickyycaudet <rickyycaudet@gmail.com>
2026-03-08 21:40:35 +0100

    Update page.tsx

--- src/app/estadisticas/page.tsx
+++ src/app/estadisticas/page.tsx
@@ -1,13 +1,16 @@
 import AuthGate from "../../components/AuthGate";
+import EstadisticasClient from "../../components/EstadisticasClient";
 
 export default function EstadisticasPage() {
   return (
     <AuthGate>
       <div>
         <h1 style={{ fontSize: 28, fontWeight: 800 }}>Estadísticas</h1>
-        <p style={{ marginTop: 8 }}>
-          Aquí verás beneficio, ROI, winrate y rendimiento por color.
+        <p style={{ marginTop: 8, marginBottom: 14 }}>
+          Aquí verás beneficio, ROI, winrate y resumen de tus apuestas.
         </p>
+
+        <EstadisticasClient />
       </div>
     </AuthGate>
   );
